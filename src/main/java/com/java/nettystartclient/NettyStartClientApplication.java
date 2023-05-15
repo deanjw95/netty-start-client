638b4d57699c0f90a59b636a52eb38db38a35cf7
deanjw95 <deanjw95@naver.com>
2023-05-15 23:01:12 +0900

    first commit;

---
+++ src/main/java/com/java/nettystartclient/NettyStartClientApplication.java
@@ -0,0 +1,13 @@
+package com.java.nettystartclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyStartClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyStartClientApplication.class, args);
+	}
+
+}
